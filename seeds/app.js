a587bcbf5d7bd8745dec33b917017ea91d29d70e
samueldusek <smldsk@protonmail.com>
2021-07-22 18:15:44 +0200

    create seed for the astronauts dummy data

---
+++ seeds/app.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const astronauts = require("./astronauts");
+console.log(astronauts[0]);
+
+const Astronaut = require("../models/astronaut");
+
+mongoose.connect("mongodb://localhost:27017/astronauts-db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+const seedDb = async () => {
+    await Astronaut.deleteMany({});
+    for(i = 0; i < astronauts.length; i++){
+        const newAstronaut = new Astronaut(astronauts[i]);
+        await newAstronaut.save();
+    }
+}
+
+seedDb().then(() => {
+    db.close();
+  });
