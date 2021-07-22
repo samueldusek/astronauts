e83141972fa8b97774a5671003c424ce5e779ed8
samueldusek <smldsk@protonmail.com>
2021-07-22 17:37:18 +0200

    create initial working template

---
+++ app.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+
+const app = express();
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "public")));
+
+mongoose.connect('mongodb://localhost:27017/astronauts-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.listen(3000, () => {
+    console.log('The app is listening on port 3000');
+})
