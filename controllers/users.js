b9268617f6006f6eee5541284079eefcaa1bfd3d
samueldusek <smldsk@protonmail.com>
2021-07-24 11:16:07 +0200

    add backend validation during user registration process

--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,29 @@
 const User = require("../models/user");
+const { validationResult } = require("express-validator");
 
 module.exports.showRegisterForm = (req, res) => {
   res.render("users/register", {
     pageTitle: "Register",
+    errors: [],
+    username: null,
+    email: null,
   });
 };
 
 module.exports.registerNewUser = async (req, res) => {
   const { username, email, password } = req.body.user;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.render("users/register", {
+      pageTitle: "Register",
+      errors: errors.array().map((error) => {
+        return { msg: error.msg, param: error.param };
+      }),
+      username: username,
+      email: email,
+    });
+  }
+
   const user = new User({ email, username });
   const registeredUser = await User.register(user, password);
   req.login(registeredUser, (error) => {
--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,52 @@ const express = require("express");
 const router = express.Router();
 const usersController = require("../controllers/users");
 const passport = require("passport");
+const User = require("../models/user");
+const { body } = require("express-validator");
 
 router
   .route("/register")
   .get(usersController.showRegisterForm)
-  .post(usersController.registerNewUser);
+  .post(
+    body("user.email")
+      .isEmail()
+      .withMessage("Please enter email in valid format example@email.com")
+      .custom((value) => {
+        return User.findOne({ email: value }).then((user) => {
+          if (user) {
+            return Promise.reject(
+              "This email has already been taken. Please use different one."
+            );
+          }
+        });
+      }),
+    body("user.username")
+      .isAlphanumeric()
+      .withMessage(
+        "Only numbers and letters are allowed in username. Please user different one."
+      )
+      .custom((value) => {
+        return User.findOne({ username: value }).then((user) => {
+          if (user) {
+            return Promise.reject(
+              "This username has already been taken. Please use different one."
+            );
+          }
+        });
+      }),
+    body("user.password")
+      .isLength({ min: 5 })
+      .withMessage(
+        "Your password it so short. Use at least 5 characters in your password."
+      ),
+    body("user.passwordConfirmation").custom((value, { req }) => {
+      if (value !== req.body.user.password) {
+        throw new Error("Password you entered are not the same. Try it again.");
+      }
+      return true;
+    }),
+    usersController.registerNewUser
+  );
 
 router
   .route("/login")
